ccd64317b922adf80533505611f4166f438977a3
Manas Patel <2233471@edu.vaniercollege.qc.ca>
2024-02-22 11:43:28 -0500

    contact us page unfinished

--- assignment1/app/views/Contact/index.php
+++ assignment1/app/views/Contact/index.php
@@ -8,7 +8,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <title>Contact Us</title>
 </head>
+<header>
+    <h1>Contact Us</h1>
+    <ul>
+        <li>Landing page</li>
+        <li>About us</li>
+        <li>Contact us</li>
+        <li>See the messages we get</li>
+    </ul>
+</header>
 <body>
-    
+    <h1>Contact us</h1>
+    <p>Wanna reach us? Write your email information and message in the following form and then submit.</p>
+    <form method='post'>
+        
+    </form>
 </body>
 </html>
