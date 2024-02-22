0bb3eba6e15b1984c4e7758fa8bfb8c30274d9f2
Manas Patel <2233471@edu.vaniercollege.qc.ca>
2024-02-22 10:54:37 -0500

    models were created

---
+++ assignment1/app/models/Count.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\models;
+
+class Count{
+    public $counter;
+
+    public function __construct(){
+
+    }
+
+    public function increment(){
+
+    }
+
+    public function write(){
+
+    }
+
+    public function __toString(){
+        
+    }
+}
---
+++ assignment1/app/models/Message.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\models;
+
+class Message{
+    public $name;
+    public $email;
+    public $IP;
+
+    public function read(){
+
+    }
+
+    public function write(){
+        
+    }
+}
