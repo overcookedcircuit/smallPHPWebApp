4e153be71f17c8b1a1cbec0c7c993021c7778c71
Manas Patel <2233471@edu.vaniercollege.qc.ca>
2024-02-22 14:42:57 -0500

    read method

--- assignment1/app/models/Message.php
+++ assignment1/app/models/Message.php
@@ -6,8 +6,20 @@ class Message{
     public $email;
     public $IP;
 
-    public function read(){
-
+    public function read() {
+        $messages = [];
+        // Read the contents of the file
+        $content = file_get_contents('messages.txt');
+        $lines = explode("\n", $content);
+        foreach ($lines as $line) {
+            $data = json_decode($line, true);
+            $message = new Message();
+            $message->email = $data['email'];
+            $message->message = $data['message'];
+            $message->IP = $data['IP'];
+            $messages[] = $message;
+        }
+        return $messages;
     }
 
     public function write(){
