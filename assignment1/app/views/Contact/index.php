1c1ae8803d1134bf18c920e22cbd7d9f6e857ff0
2230813 <jodelbriones401@gmail.com>
2024-02-23 00:36:18 -0500

    removed unessecary link O_O

--- assignment1/app/views/Contact/index.php
+++ assignment1/app/views/Contact/index.php
@@ -32,7 +32,6 @@
             <button type="button" class="form-control">Send!</button>
         </form>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-
+   
 </body>
 </html>
