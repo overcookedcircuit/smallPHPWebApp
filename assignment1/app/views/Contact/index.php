19c5bf5eebacc3122186d86fde060751b0d62c4a
overcookedcircuit <manaspatel31122004@gmail.com>
2024-02-22 23:54:05 -0500

    bootstrap works

--- assignment1/app/views/Contact/index.php
+++ assignment1/app/views/Contact/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    <title>Contact Us</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title><?= $name ?> view</title>
 </head>
 <header>
     <h1>Contact Us</h1>
@@ -21,9 +22,10 @@
     <h1>Contact us</h1>
     <p>Wanna reach us? Write your email information and message in the following form and then submit.</p>
     <form method='post'>
-        <label>Email: <input type="email" placeholder="example@email.com"></label>
-        <label>Message: <input type="textarea" placeholder="Write your message here."></label>
-        <button type="button">Send!</button>
+        <label>Email: <input type="email" placeholder="example@email.com" class="form-control"></label><br>
+        <label>Message: <textarea placeholder="Write your message here." class="form-control"></textarea></label><br>
+        <button type="button" class="btn btn-primary">Send!</button>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
